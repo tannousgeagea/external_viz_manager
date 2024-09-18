80ff666cd30940aac5dac007d745cc2734a32757
tannousgeagea <tannous.geagea@wasteant.com>
2024-09-18 17:58:51 +0200

    created superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+/bin/bash -c "python3 /home/$user/src/external_viz_manager/manage.py makemigrations"
+/bin/bash -c "python3 /home/$user/src/external_viz_manager/manage.py migrate"
+/bin/bash -c "python3 /home/$user/src/external_viz_manager/manage.py create_superuser"
+
 sudo -E supervisord -n -c /etc/supervisord.conf
